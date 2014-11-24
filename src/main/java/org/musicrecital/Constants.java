77875e10a4f36296079177bbeb455982aa5735d9
David L. Whitehurst <dlwhitehurst@me.com>
2014-11-23 23:59:06 -0500

    cleanup roles in constants file

--- src/main/java/org/musicrecital/Constants.java
+++ src/main/java/org/musicrecital/Constants.java
@@ -60,30 +60,35 @@ public final class Constants {
     public static final String REGISTERED = "registered";
 
     /**
-     * The name of the Administrator role, as specified in web.xml
+     * The name of the Administrator role
      */
     public static final String ADMIN_ROLE = "ROLE_ADMIN";
 
     /**
-     * The name of the User role, as specified in web.xml
+     * The name of the User role
      */
     public static final String USER_ROLE = "ROLE_USER";
 
     /**
-     * The name of the Listener role, as specified in web.xml
+     * The name of the Listener role
      */
     public static final String LISTENER_ROLE = "ROLE_LISTENER";
 
     /**
-     * The name of the Performer role, as specified in web.xml
+     * The name of the Performer role
      */
     public static final String PERFORMER_ROLE = "ROLE_PERFORMER";
 
     /**
-     * The name of the Manager role, as specified in web.xml
+     * The name of the MusicRecitalCoordinator role
      */
-    public static final String MANAGER_ROLE = "ROLE_MANAGER";
+    public static final String COORDINATOR_ROLE = "ROLE_COORDINATOR";
 
+    /**
+     * The name of the Accompanist role
+     */
+    public static final String ACCOMPANIST_ROLE = "ROLE_ACCOMPANIST";
+    
     /**
      * The name of the user's role list, a request-scoped attribute
      * when adding/editing a user.
