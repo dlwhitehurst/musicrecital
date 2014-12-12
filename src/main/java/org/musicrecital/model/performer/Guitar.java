b839e87c14229d9d37f7ea373c7ab29271e54b0c
David L. Whitehurst <dlwhitehurst@me.com>
2014-12-12 00:08:44 -0500

    Initial import

---
+++ src/main/java/org/musicrecital/model/performer/Guitar.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2014 MusicRecital Project
+ * 
+ * Licensed under the Apache License, Version 2.0 
+ * (the "License"); You may not use this file except 
+ * in compliance with the License. You may obtain a 
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the 
+ * License.
+ *
+ * Code developed and licensed here for org.musicrecital packaging was 
+ * formulated, copied, written, drafted, or otherwise created by 
+ * the MusicRecital project at http://musicrecital.org. This license is 
+ * freely available to the general public and is used here because 
+ * it provides the foundation for the use, duplication, and sharing 
+ * of open source software code. The cummulative code base under this 
+ * packaging was not written, maintained, or otherwise deployed by 
+ * the Apache Group. This code does not reflect the technological 
+ * growth at apache.org or the Apache Software Foundation community 
+ * collective.
+ *
+ */
+
+package org.musicrecital.model.performer;
+
+/**
+ * @author <a href="mailto:dlwhitehurst@gmail.com">David L. Whitehurst</a>
+ * @version $Id$
+ */
+public class Guitar {
+
+}
